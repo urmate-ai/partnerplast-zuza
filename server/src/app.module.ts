4ed748799954b1f73f6b008bded8e120b506dffe
Ol1mowski <olimarki123@wp.pl>
2025-11-26 17:27:09 +0100

    feat: enhance authentication flow by integrating login and registration screens, and implement loading state in RootNavigator

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AiModule } from './ai/ai.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
     AiModule,
   ],
   controllers: [AppController],
