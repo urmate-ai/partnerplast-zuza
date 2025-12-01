acddc27da5a70472392b8bb5f15a681d6d80e093
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-12-01 18:56:05 +0100

    feat: add Calendar integration with new controller and service for enhanced functionality

--- server/src/integrations/integrations.module.ts
+++ server/src/integrations/integrations.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { IntegrationsController } from './integrations.controller';
 import { GmailController } from './controllers/gmail.controller';
+import { CalendarController } from './controllers/calendar.controller';
 import { IntegrationsService } from './integrations.service';
 import { GmailService } from './services/gmail.service';
+import { CalendarService } from './services/calendar.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [IntegrationsController, GmailController],
-  providers: [IntegrationsService, GmailService],
-  exports: [IntegrationsService, GmailService],
+  controllers: [
+    IntegrationsController,
+    GmailController,
+    CalendarController,
+  ],
+  providers: [IntegrationsService, GmailService, CalendarService],
+  exports: [IntegrationsService, GmailService, CalendarService],
 })
 export class IntegrationsModule {}
